38e9d3413145e4ad9469e3af209d0e9f8301cb22
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-18 18:36:42 -0400

    update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const api = require('./public/assets/js/index.js');
 const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
-const PORT = process.env.PORT || 3001;
-
+const PORT = 3001;
+const uuid = require('./helpers/uuid');
 const app = express();
 
 app.use(express.json());
@@ -13,15 +13,16 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
+// GET * should return the index.html file.
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET * should return the index.html file.
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 // copied syntax from \UPENN-VIRT-FSF-FT-07-2022-U-LOLC\11-Express\01-Activities\24-Stu_Custom-Middleware\Solved\routes\tips.js
@@ -40,6 +41,7 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
+        id: uuid() 
       };
   
       readAndAppend(newNote, './db/db.json');
